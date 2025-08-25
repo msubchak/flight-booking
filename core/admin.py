823e621ccd7f033702c507d497e667319a34affc
Maksym Subchak <donjkee2@gmail.com>
2025-08-25 23:30:03 +0300

    feat:  Add all models, register in admin, run migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import (
+    Flight,
+    Crew,
+    Position,
+    Ticket,
+    Order,
+    Airplane,
+    AirplaneType,
+    Route,
+    Airport,
+    City,
+    Country,
+)
+
+admin.site.register(Flight)
+admin.site.register(Crew)
+admin.site.register(Position)
+admin.site.register(Ticket)
+admin.site.register(Order)
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Route)
+admin.site.register(Airport)
+admin.site.register(City)
+admin.site.register(Country)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,94 @@
 from django.db import models
 
-# Create your models here.
+from flight_booking import settings
+
+
+class Flight(models.Model):
+    route = models.ForeignKey("Route", on_delete=models.PROTECT)
+    airplane = models.ForeignKey("Airplane", on_delete=models.PROTECT)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crews = models.ManyToManyField("Crew", related_name="flights")
+
+    def __str__(self):
+        return f"Flight {self.id}, {self.departure_time} -> {self.arrival_time}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    position = models.ForeignKey("Position", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.position}"
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ticket {self.row}{self.seat} for {self.flight}"
+
+
+class Order(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user}"
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey("AirplaneType", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ManyToManyField("Airport", related_name="routes_from")
+    destination = models.ForeignKey("Airport", on_delete=models.PROTECT, related_name="routes_to")
+    distance = models.IntegerField()
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    city = models.ForeignKey("City", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.ForeignKey("Country", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
