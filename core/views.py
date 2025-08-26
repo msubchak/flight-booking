38677af6ab97d618ef4fd8fdb4cdd0b3183c3a04
Maksym Subchak <donjkee2@gmail.com>
2025-08-26 13:02:52 +0300

    feat: Implement ViewSets for models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import (
+    Flight,
+    Crew,
+    Position,
+    Ticket,
+    Order,
+    Airport,
+    AirplaneType,
+    Route,
+    City,
+    Country
+)
+from core.serializers import (
+    FlightSerializer,
+    CrewSerializer,
+    PositionSerializer,
+    TicketSerializer,
+    OrderSerializer,
+    AirplaneSerializer,
+    AirplaneTypeSerializer,
+    RouteSerializer,
+    AirportSerializer,
+    CitySerializer,
+    CountrySerializer
+)
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class  = FlightSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class PositionViewSet(viewsets.ModelViewSet):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_Class = CountrySerializer
