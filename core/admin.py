9c7e42fdf5cd78769ceeae8c0af7fb57d368f108
Maksym Subchak <donjkee2@gmail.com>
2025-08-26 14:26:26 +0300

    feat: Customize Django admin with list_display and inlines

--- core/admin.py
+++ core/admin.py
@@ -14,14 +14,57 @@ from core.models import (
     Country,
 )
 
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "city",
+    )
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "country",
+    )
+
+
+class CrewAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "position",
+    )
+
+
+class AirplaneAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "rows",
+        "seats_in_row",
+        "airplane_type",
+    )
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(Flight)
-admin.site.register(Crew)
+admin.site.register(Crew, CrewAdmin)
 admin.site.register(Position)
 admin.site.register(Ticket)
-admin.site.register(Order)
-admin.site.register(Airplane)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Airplane, AirplaneAdmin)
 admin.site.register(AirplaneType)
 admin.site.register(Route)
-admin.site.register(Airport)
-admin.site.register(City)
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(City, CityAdmin)
 admin.site.register(Country)
