74a5d9ef41b30d2c4179bc447855db0388521aea
Maksym Subchak <donjkee2@gmail.com>
2025-08-29 15:00:53 +0300

    feat: Restrict orders so users can see only their own

--- core/views.py
+++ core/views.py
@@ -124,6 +124,14 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
