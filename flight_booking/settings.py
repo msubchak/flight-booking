ee1785951540b5c5c12202835a2c22d2388506e1
Maksym Subchak <donjkee2@gmail.com>
2025-09-04 23:24:52 +0300

    chore: Add media volume to docker-compose for flight service

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/files/media"
 
 MEDIA_URL = "/media/"
 
